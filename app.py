3144ac6ef3efe940b0945c5d4abd20499471e39d
Joshua Durana <rjdurana@gmail.com>
2020-09-07 21:22:34 -0500

    Adding Extra Page

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+#CMD LIST
+#env\Scripts\activate.bat
+#set FLASK_RUN=app.py
+#set FLASK_ENV=development
+#flask run
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
 
 if __name__ == "__main__":
     app.run(debug=True)
